cd584eec0c58b6fbed645bd3d8f4cb1c810de043
[sterrevangeest] <[sterrevangeest@hotmail.nl]>
2019-04-09 11:53:33 +0200

    implemented socket.io tutorial

--- index.js
+++ index.js
@@ -4,11 +4,20 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
+var http = require("http").Server(app);
 const port = process.env.PORT || 3000;
 
 let ejs = require("ejs");
+let io = require("socket.io")(http);
+
 app.use(express.static("static"));
 
 app.get("/", (req, res) => res.render("../views/pages/index.ejs"));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+io.on("connection", function(socket) {
+  socket.on("chat message", function(msg) {
+    io.emit("chat message", msg);
+  });
+});
+
+http.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- static/index.js
+++ static/index.js
@@ -1,1 +1,15 @@
 console.log("!");
+var socket = io();
+
+$(function() {
+  var socket = io();
+  $("form").submit(function(e) {
+    e.preventDefault(); // prevents page reloading
+    socket.emit("chat message", $("#m").val());
+    $("#m").val("");
+    return false;
+  });
+  socket.on("chat message", function(msg) {
+    $("#messages").append($("<li>").text(msg));
+  });
+});
