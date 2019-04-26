ccd111239325f811af70de8c23754110b3b0f2e0
[sterrevangeest] <[sterrevangeest@hotmail.nl]>
2019-04-26 09:38:08 +0200

    update

---
+++ departures.js
@@ -0,0 +1,354 @@
+var data = [
+  {
+    links: {},
+    payload: {
+      source: "PPV",
+      departures: [
+        {
+          direction: "Rhenen",
+          name: "NS  7439",
+          plannedDateTime: "2019-04-18T11:33:00+0200",
+          plannedTimeZoneOffset: 120,
+          actualDateTime: "2019-04-18T11:33:00+0200",
+          actualTimeZoneOffset: 120,
+          plannedTrack: "3",
+          product: {
+            number: "7439",
+            categoryCode: "SPR",
+            shortCategoryName: "NS Sprinter",
+            longCategoryName: "Sprinter",
+            operatorCode: "NS",
+            operatorName: "NS",
+            type: "TRAIN"
+          },
+          trainCategory: "SPR",
+          cancelled: false,
+          routeStations: [
+            {
+              uicCode: "8400133",
+              mediumName: "Breukelen"
+            },
+            {
+              uicCode: "8400621",
+              mediumName: "Utrecht C."
+            },
+            {
+              uicCode: "8400182",
+              mediumName: "Driebergen-Zeist"
+            },
+            {
+              uicCode: "8400417",
+              mediumName: "Maarn"
+            }
+          ],
+          departureStatus: "INCOMING"
+        },
+        {
+          direction: "Rotterdam Centraal",
+          name: "NS  4041",
+          plannedDateTime: "2019-04-18T11:40:00+0200",
+          plannedTimeZoneOffset: 120,
+          actualDateTime: "2019-04-18T11:42:45+0200",
+          actualTimeZoneOffset: 120,
+          plannedTrack: "3",
+          product: {
+            number: "4041",
+            categoryCode: "SPR",
+            shortCategoryName: "NS Sprinter",
+            longCategoryName: "Sprinter",
+            operatorCode: "NS",
+            operatorName: "NS",
+            type: "TRAIN"
+          },
+          trainCategory: "SPR",
+          cancelled: false,
+          routeStations: [
+            {
+              uicCode: "8400133",
+              mediumName: "Breukelen"
+            },
+            {
+              uicCode: "8400702",
+              mediumName: "Woerden"
+            },
+            {
+              uicCode: "8400258",
+              mediumName: "Gouda"
+            }
+          ],
+          departureStatus: "INCOMING"
+        },
+        {
+          direction: "Uitgeest",
+          name: "NS  4034",
+          plannedDateTime: "2019-04-18T11:49:00+0200",
+          plannedTimeZoneOffset: 120,
+          actualDateTime: "2019-04-18T11:49:00+0200",
+          actualTimeZoneOffset: 120,
+          plannedTrack: "2",
+          product: {
+            number: "4034",
+            categoryCode: "SPR",
+            shortCategoryName: "NS Sprinter",
+            longCategoryName: "Sprinter",
+            operatorCode: "NS",
+            operatorName: "NS",
+            type: "TRAIN"
+          },
+          trainCategory: "SPR",
+          cancelled: false,
+          routeStations: [
+            {
+              uicCode: "8400074",
+              mediumName: "Bijlmer ArenA"
+            },
+            {
+              uicCode: "8400194",
+              mediumName: "Duivendrecht"
+            },
+            {
+              uicCode: "8400058",
+              mediumName: "Amsterdam C."
+            },
+            {
+              uicCode: "8400059",
+              mediumName: "Sloterdijk"
+            }
+          ],
+          departureStatus: "INCOMING"
+        },
+        {
+          direction: "Uitgeest",
+          name: "NS  7434",
+          plannedDateTime: "2019-04-18T11:58:00+0200",
+          plannedTimeZoneOffset: 120,
+          actualDateTime: "2019-04-18T11:58:00+0200",
+          actualTimeZoneOffset: 120,
+          plannedTrack: "2",
+          product: {
+            number: "7434",
+            categoryCode: "SPR",
+            shortCategoryName: "NS Sprinter",
+            longCategoryName: "Sprinter",
+            operatorCode: "NS",
+            operatorName: "NS",
+            type: "TRAIN"
+          },
+          trainCategory: "SPR",
+          cancelled: false,
+          routeStations: [
+            {
+              uicCode: "8400074",
+              mediumName: "Bijlmer ArenA"
+            },
+            {
+              uicCode: "8400194",
+              mediumName: "Duivendrecht"
+            },
+            {
+              uicCode: "8400058",
+              mediumName: "Amsterdam C."
+            },
+            {
+              uicCode: "8400059",
+              mediumName: "Sloterdijk"
+            }
+          ],
+          departureStatus: "INCOMING"
+        },
+        {
+          direction: "Rhenen",
+          name: "NS  7441",
+          plannedDateTime: "2019-04-18T12:01:00+0200",
+          plannedTimeZoneOffset: 120,
+          actualDateTime: "2019-04-18T12:01:00+0200",
+          actualTimeZoneOffset: 120,
+          plannedTrack: "3",
+          product: {
+            number: "7441",
+            categoryCode: "SPR",
+            shortCategoryName: "NS Sprinter",
+            longCategoryName: "Sprinter",
+            operatorCode: "NS",
+            operatorName: "NS",
+            type: "TRAIN"
+          },
+          trainCategory: "SPR",
+          cancelled: false,
+          routeStations: [
+            {
+              uicCode: "8400133",
+              mediumName: "Breukelen"
+            },
+            {
+              uicCode: "8400621",
+              mediumName: "Utrecht C."
+            },
+            {
+              uicCode: "8400182",
+              mediumName: "Driebergen-Zeist"
+            },
+            {
+              uicCode: "8400417",
+              mediumName: "Maarn"
+            }
+          ],
+          departureStatus: "INCOMING"
+        },
+        {
+          direction: "Rotterdam Centraal",
+          name: "NS  4043",
+          plannedDateTime: "2019-04-18T12:10:00+0200",
+          plannedTimeZoneOffset: 120,
+          actualDateTime: "2019-04-18T12:10:00+0200",
+          actualTimeZoneOffset: 120,
+          plannedTrack: "3",
+          product: {
+            number: "4043",
+            categoryCode: "SPR",
+            shortCategoryName: "NS Sprinter",
+            longCategoryName: "Sprinter",
+            operatorCode: "NS",
+            operatorName: "NS",
+            type: "TRAIN"
+          },
+          trainCategory: "SPR",
+          cancelled: false,
+          routeStations: [
+            {
+              uicCode: "8400133",
+              mediumName: "Breukelen"
+            },
+            {
+              uicCode: "8400702",
+              mediumName: "Woerden"
+            },
+            {
+              uicCode: "8400258",
+              mediumName: "Gouda"
+            }
+          ],
+          departureStatus: "INCOMING"
+        },
+        {
+          direction: "Uitgeest",
+          name: "NS  4036",
+          plannedDateTime: "2019-04-18T12:19:00+0200",
+          plannedTimeZoneOffset: 120,
+          actualDateTime: "2019-04-18T12:19:00+0200",
+          actualTimeZoneOffset: 120,
+          plannedTrack: "2",
+          product: {
+            number: "4036",
+            categoryCode: "SPR",
+            shortCategoryName: "NS Sprinter",
+            longCategoryName: "Sprinter",
+            operatorCode: "NS",
+            operatorName: "NS",
+            type: "TRAIN"
+          },
+          trainCategory: "SPR",
+          cancelled: false,
+          routeStations: [
+            {
+              uicCode: "8400074",
+              mediumName: "Bijlmer ArenA"
+            },
+            {
+              uicCode: "8400194",
+              mediumName: "Duivendrecht"
+            },
+            {
+              uicCode: "8400058",
+              mediumName: "Amsterdam C."
+            },
+            {
+              uicCode: "8400059",
+              mediumName: "Sloterdijk"
+            }
+          ],
+          departureStatus: "INCOMING"
+        },
+        {
+          direction: "Uitgeest",
+          name: "NS  7436",
+          plannedDateTime: "2019-04-18T12:28:00+0200",
+          plannedTimeZoneOffset: 120,
+          actualDateTime: "2019-04-18T12:28:00+0200",
+          actualTimeZoneOffset: 120,
+          plannedTrack: "2",
+          product: {
+            number: "7436",
+            categoryCode: "SPR",
+            shortCategoryName: "NS Sprinter",
+            longCategoryName: "Sprinter",
+            operatorCode: "NS",
+            operatorName: "NS",
+            type: "TRAIN"
+          },
+          trainCategory: "SPR",
+          cancelled: false,
+          routeStations: [
+            {
+              uicCode: "8400074",
+              mediumName: "Bijlmer ArenA"
+            },
+            {
+              uicCode: "8400194",
+              mediumName: "Duivendrecht"
+            },
+            {
+              uicCode: "8400058",
+              mediumName: "Amsterdam C."
+            },
+            {
+              uicCode: "8400059",
+              mediumName: "Sloterdijk"
+            }
+          ],
+          departureStatus: "INCOMING"
+        },
+        {
+          direction: "Rhenen",
+          name: "NS  7443",
+          plannedDateTime: "2019-04-18T12:33:00+0200",
+          plannedTimeZoneOffset: 120,
+          actualDateTime: "2019-04-18T12:33:00+0200",
+          actualTimeZoneOffset: 120,
+          plannedTrack: "3",
+          product: {
+            number: "7443",
+            categoryCode: "SPR",
+            shortCategoryName: "NS Sprinter",
+            longCategoryName: "Sprinter",
+            operatorCode: "NS",
+            operatorName: "NS",
+            type: "TRAIN"
+          },
+          trainCategory: "SPR",
+          cancelled: false,
+          routeStations: [
+            {
+              uicCode: "8400133",
+              mediumName: "Breukelen"
+            },
+            {
+              uicCode: "8400621",
+              mediumName: "Utrecht C."
+            },
+            {
+              uicCode: "8400182",
+              mediumName: "Driebergen-Zeist"
+            },
+            {
+              uicCode: "8400417",
+              mediumName: "Maarn"
+            }
+          ],
+          departureStatus: "INCOMING"
+        }
+      ]
+    },
+    meta: {}
+  }
+];
--- index.js
+++ index.js
@@ -1,34 +1,215 @@
-console.log("test");
+"user strict";
 
 require("dotenv").config();
 
 const express = require("express");
 const app = express();
 var http = require("http").Server(app);
-const port = process.env.PORT || 3000;
-let emoji = require("node-emoji");
+const port = process.env.PORT || 8000;
+const crypto = require("crypto");
 let ejs = require("ejs");
 let io = require("socket.io")(http);
+var request = require("request");
+var bodyParser = require("body-parser");
 
+const secret = "abcdefg";
+
+const hash = crypto.createHmac("sha256", secret).digest("hex");
+console.log(hash);
+
+var users = [
+  {
+    voornaam: "Henk",
+    achternaam: "Visser",
+    gebruikersnaam: "henkdevries",
+    wachtwoord: "vis",
+    vrienden: [
+      {
+        gebruikersnaam: "jandeboer"
+      }
+    ],
+    reis: [
+      {
+        direction: "Rhenen",
+        plannedDateTime: "2019-04-25T15:33:00+0200",
+        trainType: "Sprinter",
+        plannedTrack: "3",
+        tripId: "NS7455"
+      }
+    ]
+  }
+];
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 app.use(express.static("static"));
 
-app.get("/", (req, res) => res.render("../views/pages/index.ejs"));
+app.post("/profile", (req, res) => {
+  //gebruikersdata opslaan in "database"
+  var userData = req.body;
 
-io.on("connection", socket => {
-  var msgCount = [];
-  socket.on("chat message", msg => {
-    var msg = msg.split(" ").map(word => {
-      msgCount++;
-      return emoji.get(word) || word;
-    });
-    console.log(msgCount);
+  var test = users.map(function(user) {
+    console.log(user.gebruikersnaam);
+    console.log(userData.gebruikersnaam);
+    if (user.gebruikersnaam === userData.gebruikersnaam) {
+      var data = "gebruikersnaam is al in gebruik";
+      res.render("../views/pages/createProfile.ejs", {
+        data: data
+      });
+    } else {
+      if (user.gebruikersnaam === userData.gebruikersnaam) {
+      } else {
+        users.push(userData);
+        console.log(users);
+        var data = userData;
+        res.render("../views/pages/profile.ejs", {
+          data: data
+        });
+      }
+    }
+  });
+});
 
-    var msg = msg
-      .toString()
-      .replace(/,/g, " ")
-      .replace(/:/g, "");
+app.get("/selectStation", (req, res) => {
+  var options = {
+    url:
+      "https://gateway.apiportal.ns.nl/public-reisinformatie/api/v2/stations",
+    headers: {
+      "Content-Type": "application/json",
+      "Ocp-Apim-Subscription-Key": "a25da04c7ab94cf1bf6a3663aa4fb712"
+    }
+  };
 
-    io.emit("chat message", msg, msgCount);
+  function callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var data = JSON.parse(body);
+      var data = data.payload;
+
+      var results = data.map(station => {
+        return { stationName: station.namen.lang, stationId: station.UICCode };
+      });
+      console.log(results);
+
+      res.render("../views/pages/selectStation.ejs", {
+        data: results
+      });
+    } else {
+      console.log(error);
+    }
+  }
+  request(options, callback);
+});
+
+app.post("/vertrektijden", (req, res) => {
+  var stationId = req.body.stationId.split(" ");
+  console.log(stationId);
+
+  var options = {
+    url:
+      "https://gateway.apiportal.ns.nl/public-reisinformatie/api/v2/departures?maxJourneys=25&lang=nl&uicCode=" +
+      stationId[0],
+    headers: {
+      "Content-Type": "application/json",
+      "Ocp-Apim-Subscription-Key": "a25da04c7ab94cf1bf6a3663aa4fb712"
+    }
+  };
+
+  function callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var data = JSON.parse(body);
+
+      var data = data.payload.departures;
+      console.log(data);
+      // var departures = data.map(item => item.payload.departures);
+      var departures = data.map(item => {
+        return {
+          direction: item.direction,
+          plannedDateTime: item.plannedDateTime,
+          trainType: item.product.longCategoryName,
+          plannedTrack: item.plannedTrack,
+          tripId: item.name.replace(/\s/g, "")
+        };
+      });
+
+      res.render("../views/pages/trip.ejs", {
+        data: stationId[1],
+        departures: departures
+      });
+      console.log(departures);
+    } else {
+      console.log(error);
+    }
+  }
+  request(options, callback);
+});
+
+app.get("/createProfile", (req, res) => {
+  res.render("../views/pages/createProfile.ejs", {
+    data: ""
+  });
+});
+
+// var results = sporen.map(station => {
+//   return { stationName: station.namen.lang, stationId: station.UICCode };
+// });
+// var departures = departures.map(item => item.payload.departures);
+// var departures = departures.map(item => {
+//   return item.map(item => {
+//     return {
+//       direction: item.direction,
+//       plannedDateTime: item.plannedDateTime,
+//       trainType: item.product.longCategoryName,
+//       plannedTrack: item.plannedTrack,
+//       tripId: item.name.replace(/\s/g, "")
+//     };
+//   });
+// });
+
+// app.post("/gebruiker", (req, res) => {
+//   var name = req.body.name;
+//   db.push({ clientName: name, clientId: 1 });
+//   res.render("../views/pages/index.ejs", {
+//     data: sporen
+//   });
+// });
+
+app.post("/vertrektijden", function(req, res) {
+  res.render("../views/pages/trip.ejs", {
+    data: req.body.stationName,
+    departures: departures
+  });
+});
+
+app.get("/reis/:id", function(req, res) {
+  var roomId = req.params.id;
+  console.log(roomId);
+  //getTrip
+
+  res.render("../views/pages/reis.ejs", {
+    data: req.params.id
+  });
+});
+
+const tech = io.of("/tech");
+
+tech.on("connection", socket => {
+  socket.on("join", data => {
+    socket.join(data.room);
+    tech.in(data.room).emit("message", `New user joined ${data.room} room!`);
+  });
+  socket.on("message", data => {
+    console.log("msg " + data.msg);
+    tech.in(data.room).emit("message", data.msg);
+  });
+
+  socket.on("disconnect", data => {
+    console.log("user disconnected");
+    tech.in(data.room).emit("message", "user disconnected");
+    tech.emit("message", "user disconnected");
   });
 });
 
