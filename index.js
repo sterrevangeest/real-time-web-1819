394954103dcb062003651cccbfec081768575ecf
[sterrevangeest] <[sterrevangeest@hotmail.nl]>
2019-05-24 11:12:58 +0200

    update

--- index.js
+++ index.js
@@ -1,59 +1,14 @@
 "use strict";
 console.log("test");
 
-var results = [
-  { stationName: "Abcoude", stationId: "8400047" },
-  { stationName: "Arnhem Centraal", stationId: "8400071" },
-  { stationName: "Aachen Hbf", stationId: "8015345" },
-  { stationName: "Arnhem Velperpoort", stationId: "8400072" },
-  { stationName: "Arnhem Presikhaaf", stationId: "8400075" },
-  { stationName: "Arnhem Zuid", stationId: "8400227" },
-  { stationName: "Aime-la-Plagne", stationId: "8774176" },
-  { stationName: "Aix-en-Provence TGV", stationId: "8731901" },
-  { stationName: "Arkel", stationId: "8400068" },
-  { stationName: "Akkrum", stationId: "8400049" },
-  { stationName: "Albertville", stationId: "8774164" },
-  { stationName: "Almere Centrum", stationId: "8400080" },
-  { stationName: "Almere Buiten", stationId: "8400081" },
-  { stationName: "Almere Muziekwijk", stationId: "8400082" },
-  { stationName: "Almere Oostvaarders", stationId: "8400226" },
-  { stationName: "Almere Parkwijk", stationId: "8400104" },
-  { stationName: "Amersfoort", stationId: "8400055" },
-  { stationName: "Amersfoort Schothorst", stationId: "8400054" },
-  { stationName: "Almelo", stationId: "8400051" },
-  { stationName: "Alkmaar", stationId: "8400050" },
-  { stationName: "Almelo de Riet", stationId: "8400520" },
-  { stationName: "Alkmaar Noord", stationId: "8400052" },
-  { stationName: "Anna Paulowna", stationId: "8400065" },
-  { stationName: "Antwerpen-Noorderdokken", stationId: "8821089" },
-  { stationName: "Apeldoorn", stationId: "8400066" },
-  { stationName: "Apeldoorn De Maten", stationId: "8400233" },
-  { stationName: "Apeldoorn Osseveld", stationId: "8400229" },
-  { stationName: "Appingedam", stationId: "8400067" },
-  { stationName: "Alphen a/d Rijn", stationId: "8400053" },
-  { stationName: "Arnemuiden", stationId: "8400069" },
-  { stationName: "Amsterdam Amstel", stationId: "8400057" },
-  { stationName: "Amsterdam Bijlmer ArenA", stationId: "8400074" },
-  { stationName: "Amsterdam Centraal", stationId: "8400058" },
-  { stationName: "Amsterdam Lelylaan", stationId: "8400079" },
-  { stationName: "Amsterdam Muiderpoort", stationId: "8400060" },
-  { stationName: "Amsterdam Zuid", stationId: "8400061" },
-  { stationName: "Amsterdam Holendrecht", stationId: "8400231" },
-  { stationName: "Assen", stationId: "8400073" },
-  { stationName: "Amsterdam Sloterdijk", stationId: "8400059" },
-  { stationName: "Amsterdam Science Park", stationId: "8400235" },
-  { stationName: "Aalten", stationId: "8400045" }
-];
-
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 const http = require("http").Server(app);
-
 const bodyParser = require("body-parser");
-
 const uniqid = require("uniqid");
 const request = require("request");
+
 const dotenv = require("dotenv");
 require("dotenv").config();
 
@@ -79,16 +34,16 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database();
-
+app.set("view engine", "ejs");
 app.use(express.static("static"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.render("../views/pages/index.ejs");
+  res.render("../views/pages/index");
 });
 
 app.get("/signup", (req, res) => {
-  res.render("../views/pages/singup.ejs");
+  res.render("../views/pages/singup");
 });
 
 app.post("/signup", (req, res) => {
@@ -99,84 +54,260 @@ app.post("/signup", (req, res) => {
   firebase
     .auth()
     .createUserWithEmailAndPassword(email, password)
-    .catch(error => console.log(error));
-
-  firebase.auth().onAuthStateChanged(firebaseUser => {
-    if (firebaseUser) {
-      let userId = firebaseUser.uid;
+    .then(user => {
+      var currentUserId = firebase.auth().currentUser.uid;
       firebase
         .database()
-        .ref(userId)
+        .ref(currentUserId)
         .set({
-          userId: userId,
+          userId: currentUserId,
           password: password,
           name: name,
           email: email,
           connections: [
             {
-              name: "Henk de Boer",
-              email: "henk123@hotmail.nl",
-              userId: "wzkv2PULDAM7oIRy4tJJf1XFlIa2"
+              name: "Henk",
+              email: "henk1@hotmail.nl",
+              userId: "yagbKHZpZLSwYgOlR9MLVCcTwH92"
             }
           ],
           invitations: []
-        })
-        .then(user => {
-          console.log(user);
-          res.render("../views/pages/profile.ejs", {
-            user: user
-          });
         });
-    } else {
-      console.log("not logged in");
-    }
-  });
-});
-
-app.post("/login", (req, res) => {
-  console.log("PARAMS", req.params);
-  const email = req.body.email;
-  console.log(email);
-  const password = req.body.password;
-  console.log(password);
-
-  firebase
-    .auth()
-    .signInWithEmailAndPassword(email, password)
-    .catch(error => console.log(error));
-
-  firebase.auth().onAuthStateChanged(firebaseUser => {
-    if (firebaseUser) {
+    })
+    .then(response => {
+      var currentUserId = firebase.auth().currentUser.uid;
       firebase
         .database()
         .ref()
-        .child(firebaseUser.uid)
+        .child(currentUserId)
         .on("value", snapshot => {
           console.log(snapshot.val());
           res.render("../views/pages/profile.ejs", {
             user: snapshot.val(),
-            data: results,
-
+            data: [],
             invite: []
           });
         });
-    } else {
-      console.log("not logged in");
-    }
-  });
+    })
+    .catch(error => console.log(error));
+});
+
+app.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then(user => {
+      var currentUserId = firebase.auth().currentUser.uid;
+      firebase
+        .database()
+        .ref()
+        .child(currentUserId)
+        .on("value", snapshot => {
+          console.log(snapshot.val());
+          var options = {
+            url:
+              "https://gateway.apiportal.ns.nl/public-reisinformatie/api/v2/stations",
+            headers: {
+              "Content-Type": "application/json",
+              "Ocp-Apim-Subscription-Key": "a25da04c7ab94cf1bf6a3663aa4fb712"
+            }
+          };
+
+          function callback(error, response, body) {
+            if (!error && response.statusCode == 200) {
+              var data = JSON.parse(body);
+              var data = data.payload;
+
+              var results = data.map(station => {
+                return {
+                  stationName: station.namen.lang,
+                  stationId: station.UICCode
+                };
+              });
+              res.render("../views/pages/profile.ejs", {
+                user: snapshot.val(),
+                data: results,
+                invite: []
+              });
+            } else {
+              console.log(error);
+            }
+          }
+          request(options, callback);
+        });
+    })
+    .catch(error => console.log(error));
 });
 
 app.post("/vrienduitnodigen/:email", (req, res) => {
   var inviteThisEmail = req.params.email;
-  console.log(inviteThisEmail);
+});
+
+app.post("/vertrektijden", function(req, res) {
+  //console.log(req.body.stationId);
+  var stationName = req.body.stationId.split(" ")[1];
+  var stationId = req.body.stationId.split(" ")[0];
+  var options = {
+    url:
+      "https://gateway.apiportal.ns.nl/public-reisinformatie/api/v2/departures?maxJourneys=25&lang=nl&uicCode=" +
+      stationId,
+    headers: {
+      "Content-Type": "application/json",
+      "Ocp-Apim-Subscription-Key": "a25da04c7ab94cf1bf6a3663aa4fb712"
+    }
+  };
+
+  function callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var data = JSON.parse(body);
+
+      var data = data.payload.departures;
+
+      // var departures = data.map(item => item.payload.departures);
+      var departures = data.map(item => {
+        return {
+          direction: item.direction,
+          plannedDateTime: item.plannedDateTime,
+          trainType: item.product.longCategoryName,
+          plannedTrack: item.plannedTrack,
+          tripId: item.name.replace(/\s/g, "")
+        };
+      });
+
+      res.render("../views/pages/vertrektijden.ejs", {
+        data: stationName,
+        stationId: stationId,
+        departures: departures
+      });
+    } else {
+      console.log(error);
+    }
+  }
+  request(options, callback);
+});
+
+app.get("/reis/:id", function(req, res) {
+  var roomId = req.params.id;
+  //console.log(roomId);
+  let usersInSameRoom = [];
+  let currentUserId = firebase.auth().currentUser.uid;
+
   firebase
     .database()
     .ref()
-    .child(inviteThisEmail)
+    .child(currentUserId)
+    .update({ roomId: roomId });
+
+  firebase
+    .database()
+    .ref()
     .once("value")
     .then(function(snapshot) {
-      console.log(snapshot.val());
+      snapshot.forEach(function(childSnapshot) {
+        var key = childSnapshot.key;
+        var hasRoomId = childSnapshot.hasChild("roomId"); // true
+        if (hasRoomId === true) {
+          console.log("ROOMID", childSnapshot.val().roomId);
+          if (childSnapshot.val().roomId === roomId) {
+            console.log(childSnapshot.val().roomId);
+            console.log(roomId);
+            console.log("ook nog zelfd room ID");
+            usersInSameRoom.push(childSnapshot.val());
+            console.log(usersInSameRoom);
+          } else {
+            console.log("NIET ZELFDE ROOM ID");
+          }
+          //check of ze hetzelfde nummer hebben en het vrienden zijn
+        } else {
+          console.log("niet gelukt om een ROOMid te vinden");
+        }
+      });
+
+      var possibleFriends = usersInSameRoom.map(user => {
+        if (user.userId !== currentUserId) {
+          //haal alle andere user Ids op
+
+          if (user.userId !== undefined) {
+            return user.userId;
+          }
+        }
+      });
+
+      firebase
+        .database()
+        .ref()
+        .child(currentUserId)
+        .once("value")
+        .then(function(snapshot) {
+          console.log(snapshot.val());
+          var user = snapshot.val();
+          var friend = user.connections.map(item => {
+            console.log(item);
+            for (var i = 0; i < possibleFriends.length; i++) {
+              if (item.userId === possibleFriends[i]) {
+                console.log("VRIENDON");
+                return possibleFriends[i];
+              } else {
+                console.log("geen vriendon");
+              }
+            }
+          });
+          console.log(friend);
+          return friend;
+        })
+        .then(friend => {
+          for (var i = 0; i < friend.length; i++) {
+            firebase
+              .database()
+              .ref(friend[i])
+              .once("value")
+              .then(function(snapshot) {
+                res.render("../views/pages/medereizigers", {
+                  data: roomId,
+                  friendsInSameRoom: [snapshot.val()], //data van de friend in dezelfde room
+                  usersInSameRoom: possibleFriends
+                });
+              });
+          }
+        })
+        .catch(error => console.log(error));
     });
+});
+
+app.get("/chat/:userId", function(req, res) {
+  res.render("../views/pages/chat.ejs", {
+    data: req.params
+  });
+});
+
+const tech = io.of("/tech");
+
+tech.on("connection", socket => {
+  socket.on("join", data => {
+    socket.join(data.room);
+    // tech.in(data.room).emit("message", `New user joined ${data.room} room!`);
+  });
+  socket.on("message", data => {
+    console.log("msg " + data.msg);
+    tech.in(data.room).emit("message", data.msg);
+  });
+
+  socket.on("disconnect", data => {
+    console.log("user disconnected");
+    tech.in(data.room).emit("message", "user disconnected");
+    //tech.emit("message", "user disconnected");
+  });
+});
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    console.log("user is signed in");
+  } else {
+    console.log("user is signed out");
+  }
 });
 
 http.listen(port, () => console.log(`Example app listening on port ${port}!`));
