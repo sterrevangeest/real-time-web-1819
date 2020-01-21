981496cb6969ea1ca41c798ad4be9c53320f64b5
[sterrevangeest] <[sterrevangeest@hotmail.nl]>
2020-01-21 22:27:29 +0100

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const bodyParser = require("body-parser");
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 const app = express();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
